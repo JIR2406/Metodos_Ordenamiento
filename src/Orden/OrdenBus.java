db28b90b0dde8b5e83fe5b113033f854476a6133
JIR <jairgarduno47@gmail.com>
2022-11-30 20:06:22 -0600

    Metodo Radix

--- src/Orden/OrdenBus.java
+++ src/Orden/OrdenBus.java
@@ -45,19 +45,46 @@ public class OrdenBus {
         }
 
     }
-    public void radix(ColaCir a, int t){
-        ColaCir cola[]=new ColaCir[10];
-        for (int i = 0; i < cola.length-1; i++) {
-            cola[i]=new ColaCir(t);
+
+    public void radix(ColaCir a, int t) {
+        ColaCir cola[] = new ColaCir[10];
+        for (int i = 0; i < cola.length - 1; i++) {
+            cola[i] = new ColaCir(t);
         }
-        while(!a.empty()){
-            int aux=(int)a.remove();
-            int resul=aux/10;
-            if (resul==0) {
+        while (!a.empty()) {
+            int aux = (int) a.remove();
+            int unidad = aux % 10;
+            int decena = (aux % 100 - aux % 10) / 10;
+            int centena = (aux % 1000 - aux % 100) / 100;
+            if (decena == 0) {
                 cola[0].insert(aux);
+            } else if (decena == 1) {
+                cola[1].insert(aux);
+            } else if (decena == 2) {
+                cola[2].insert(aux);
+            } else if (decena == 3) {
+                cola[3].insert(aux);
+            } else if (decena == 4) {
+                cola[4].insert(aux);
+            } else if (decena == 5) {
+                cola[5].insert(aux);
+            } else if (decena == 6) {
+                cola[6].insert(aux);
+            } else if (decena == 7) {
+                cola[7].insert(aux);
+            } else if (decena == 8) {
+                cola[8].insert(aux);
+            } else if (decena == 9) {
+                cola[9].insert(aux);
+            }
+        }
+        for (int i = 0; i < cola.length - 1; i++) {
+            while (!cola[i].empty()) {
+                a.insert(cola[i].remove());
             }
         }
     }
+
     public void desplegar(int a[]) {
         for (int i = 0; i < a.length; i++) {
             if (i != 0) {
